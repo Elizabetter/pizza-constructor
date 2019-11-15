a5f48524692e094bd07b8b9a67946cdb254f4085
Elizabetter <isayl000@mail.ru>
2019-11-15 21:30:44 +0300

    layout

---
+++ main.js
@@ -0,0 +1,69 @@
+function count() {
+    let cost = document.getElementById("cost");
+    let plus_cost = document.getElementById("plusCost");
+    cost.innerHTML = +cost.innerHTML + +plus_cost.innerHTML;
+}
+
+toppings =
+[
+    {
+        name: "pepperoni",
+        cost : 1.80,
+        calories: 250
+    },
+    {
+        name: "anchovy",
+        cost : 1.80,
+        calories: 250
+    },
+    {
+        name: "basil",
+        cost : 1.80,
+        calories: 250
+    },
+    {
+        name: "chili",
+        cost : 1.80,
+        calories: 250
+    },
+    {
+        name: "mushroom",
+        cost : 1.80,
+        calories: 250
+    },
+    {
+        name: "onion",
+        cost : 1.80,
+        calories: 250
+    },
+    {
+        name: "mozzarella",
+        cost : 1.80,
+        calories: 250
+    },
+    {
+        name: "tomato",
+        cost : 1.80,
+        calories: 250
+    },
+    {
+        name: "sweetcorn",
+        cost : 1.80,
+        calories: 250
+    },
+    {
+        name: "prawn",
+        cost : 1.80,
+        calories: 250
+    },
+    {
+        name: "pepper",
+        cost : 1.80,
+        calories: 250
+    },
+    {
+        name: "olive",
+        cost : 1.80,
+        calories: 250
+    }
+];
